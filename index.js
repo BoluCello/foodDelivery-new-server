139d94aa4ab631c43c18cbb6827dd9d61e9123f2
Boluwatife <akintundeboluwatife@gmail.com>
2024-10-17 10:55:46 +0100

    another update

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ import FoodRoutes from "./routes/Food.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: 'https://food-delivery-new-client.vercel.app', // Your frontend URL
+    methods: ['GET', 'POST'],
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
